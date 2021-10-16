b2d72960ac209b2069485a3efbc90cd4eb709563
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-10-16 03:15:58 -0300

    fiz o get all species

--- src/controllers/SpeciesController.js
+++ src/controllers/SpeciesController.js
@@ -2,6 +2,17 @@ const connection = require("../database/connection");
 
 module.exports = {
     async listAllSpecies(req, res) {
-        return res.json({ "Res": "OK" })
+        const { jwt } = req.headers;
+        const species = await connection("Specie")
+            .select("id_specie", "scientific_name")
+        let data = [];
+        for (i in species) {
+            const photos = await connection("Specie_Photo").select("img_specie").where("fk_id_specie", species[i].id_specie)
+
+            let sp_dt = { "specie_details": species[i], "specie_images": photos }
+            data.push(sp_dt);
+        }
+
+        return res.json({ "Species": data })
     }
 }
