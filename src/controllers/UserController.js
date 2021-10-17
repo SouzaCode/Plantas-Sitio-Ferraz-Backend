cac31f94efd0a0df5c3c8e8210cf6750c5ea8035
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-10-17 17:07:12 -0300

    fiz a rota de registro

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,6 +3,15 @@ const connection = require("../database/connection");
 module.exports = {
     async registerNewUser(req, res) {
         const { email, hash_password, name } = req.body;
-        return res.json({ "OI": [email, hash_password, name] })
+        const previousUser = await connection("User").select("name").where("email", email);
+        if (previousUser.length) {
+            return res.status(400).json({ "Error": "Email already registered. Please use another one." })// Verificar este código
+        }
+        const newUser = await connection("User").insert({
+            "email": email,
+            "name": name,
+            "hash_password": hash_password
+        })
+        return res.json({ "Response": "User created successfully! Please Login to get your access token." })
     }
 }
