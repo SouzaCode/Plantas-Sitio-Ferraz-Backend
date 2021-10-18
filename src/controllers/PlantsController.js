bcbf6ec428e936b73ae9ae40bbdf47f48ac5253c
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-10-18 14:16:34 -0300

    adicionei o join das infos do user e da especie

--- src/controllers/PlantsController.js
+++ src/controllers/PlantsController.js
@@ -8,14 +8,14 @@ module.exports = {
         let decodedJWT;
         let userQuery = ""
         if (users) {
-            userQuery = " where fk_id_user in (" + users + ")"
+            userQuery = " where Plant.fk_id_user in (" + users + ")"
         }
         let speciesQuery = "";
         if (species) {
             if (users) {
-                speciesQuery = " and fk_id_specie in (" + species + ")"
+                speciesQuery = " and Plant.fk_id_specie in (" + species + ")"
             } else {
-                speciesQuery = " where fk_id_specie in (" + species + ")"
+                speciesQuery = " where Plant.fk_id_specie in (" + species + ")"
             }
 
         }
@@ -32,29 +32,31 @@ module.exports = {
                 return res.status(401).json({ "Error": "No token provided." })
             }
             if (users || species) {
-                privateQuery = " and is_private = 0 or fk_id_user = " + decodedJWT.id_user;
+                privateQuery = " and Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
             } else {
-                privateQuery = " where is_private = 0 or fk_id_user = " + decodedJWT.id_user;
+                privateQuery = " where Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
             }
             privateQuery = privateQuery
         } else {
 
             if (users) {
                 if (species)
-                    privateQuery = " and is_private = 0"
+                    privateQuery = " and Plant.is_private = 0"
                 else
-                    privateQuery = " where is_private = 0";
+                    privateQuery = " where Plant.is_private = 0";
             } else {
-                if (species)
-                    privateQuery = " and is_private = 0 or fk_id_user = " + decodedJWT.id_user;
-                else {
-                    privateQuery = " where is_private = 0 or fk_id_user = " + decodedJWT.id_user;
+                if (decodedJWT) {
+                    if (species)
+                        privateQuery = " and Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
+                    else {
+                        privateQuery = " where Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
+                    }
                 }
             }
 
 
         }
-        let final_query = "select * from Plant" + userQuery + speciesQuery + privateQuery + " limit 10 offset " + pageNumber;
+        let final_query = "select Plant.*, Specie.scientific_name, User.name as user_name from Plant join Specie on Specie.id_specie = Plant.fk_id_specie join User on User.id_user = Plant.fk_id_user" + userQuery + speciesQuery + privateQuery + " limit 10 offset " + pageNumber;
         console.log(final_query);
         const plants = await connection.raw(final_query)
         let plantsData = [];
