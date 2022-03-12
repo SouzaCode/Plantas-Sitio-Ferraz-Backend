8437ae3f6adff7e915105818a21f2d2d14f2c0cf
CodeWracker <dev.rodrigofs@gmail.com>
2022-03-12 16:23:03 -0300

    arrumei o getall

--- src/controllers/PlantsController.js
+++ src/controllers/PlantsController.js
@@ -5,62 +5,40 @@ const randomString = require('random-base64-string')
 module.exports = {
     async getAllPlants(req, res) {
         const { species, users, token, is_private, page } = req.headers;
+
         let pageNumber = page ? parseInt(page) * 10 : 0;
         let decodedJWT;
         let userQuery = ""
-        if (users) {
-            userQuery = " where Plant.fk_id_user in (" + users + ")"
-        }
-        let speciesQuery = "";
-        if (species) {
-            if (users) {
-                speciesQuery = " and Plant.fk_id_specie in (" + species + ")"
-            } else {
-                speciesQuery = " where Plant.fk_id_specie in (" + species + ")"
-            }
+        const subquery = connection("Plant")
+            .select("Plant.*", "Specie.scientific_name")
+            .join("Specie", "Specie.id_specie", "Plant.fk_id_specie")
 
+        if (species && species != "") {
+            subquery.whereIn("Plant.fk_id_specie", species.split(','))
         }
-        /*
-        if (token) {
-            try {
-                decodedJWT = jwt.verify(token, process.env.SECRET_JWT);
-            } catch (err) {
-                return res.status(401).json({ "Error": "Invalid Token with message '" + err + "'" })
-            }
-        }*/
-        let privateQuery = ""
         if (is_private == 1) {
+            if (token) {
+                try {
+                    decodedJWT = jwt.verify(token, process.env.SECRET_JWT);
+                } catch (err) {
+                    return res.status(401).json({ "Error": "Invalid Token with message '" + err + "'" })
+                }
+            }
             if (!decodedJWT) {
                 return res.status(401).json({ "Error": "No token provided." })
             }
-            if (users || species) {
-                privateQuery = " and Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
-            } else {
-                privateQuery = " where Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
-            }
-            privateQuery = privateQuery
+            subquery.where("Plant.fk_id_user", decodedJWT.id_user).where("is_private", 1)
         } else {
-
-            if (users) {
-                if (species)
-                    privateQuery = " and Plant.is_private = 0"
-                else
-                    privateQuery = " where Plant.is_private = 0";
-            } else {
-                if (decodedJWT) {
-                    if (species)
-                        privateQuery = " and Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
-                    else {
-                        privateQuery = " where Plant.is_private = 0 or Plant.fk_id_user = " + decodedJWT.id_user;
-                    }
-                }
+            if (users && users != "") {
+                subquery.whereIn("Plant.fk_id_user", users.split(','))
             }
-
-
+            subquery.where("Plant.is_private", 0)
         }
-        let final_query = "select Plant.*, Specie.scientific_name, User.name as user_name from Plant join Specie on Specie.id_specie = Plant.fk_id_specie join User on User.id_user = Plant.fk_id_user" + userQuery + speciesQuery + privateQuery + " limit 10 offset " + pageNumber;
-        console.log(final_query);
-        const plants = await connection.raw(final_query)
+        subquery.limit(10).offset(pageNumber)
+        console.log(subquery.toString())
+
+        const plants = await subquery
+        console.log(plants);
         let plantsData = [];
         if (plants.length) {
             for (i in plants) {
